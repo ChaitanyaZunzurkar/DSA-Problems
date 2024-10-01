b957c4ca2bbd44dde9ef1220edf901c0dc559d2b
Chaitanya Zunzurkar <chaitanyazunzurkar09@gmail.com>
2024-10-02 01:01:16 +0530

    Implementation of linkedlist using array

---
+++ Linkedlist/ImplementingLinkedlistUsingArray.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+        int val;
+        Node * next;
+
+        Node(int val) {
+            this->val = val;
+            this->next = nullptr;
+        }
+};
+
+Node * arrToLinkedList(int arr[] , int n) {
+    Node * head = new Node(arr[0]);
+    Node * curr = head;
+
+    for(int i = 1; i < n; i++) {
+        Node * temp = new Node(arr[i]);
+        curr->next = temp;
+        curr = temp;
+    }
+    return head;
+}
+
+void printLinkedlist(Node * root) {
+    while(root != nullptr) {
+        cout << root->val << " ";
+        root = root->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int n = 7;
+    int arr[] = {10 , 20 , 30 , 40 , 50 , 60 , 70};
+    Node * root = arrToLinkedList(arr , 7);
+    printLinkedlist(root);
+    return 0;
+}
