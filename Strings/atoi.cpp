b4b3a8e49b773e53617fe2c82ddd36387b7c1620
Chaitanya Zunzurkar <chaitanyazunzurkar09@gmail.com>
2025-06-06 23:48:53 +0530

    Topic: string , Problem : atoi function implementation

--- Strings/atoi.cpp
+++ Strings/atoi.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include <climits>  // For INT_MAX and INT_MIN
+#include <climits>  
 using namespace std;
 
 int myAtoi(string s) {
@@ -8,20 +8,16 @@ int myAtoi(string s) {
     long result = 0;
     int sign = 1;
 
-    // 1. Skip leading whitespaces
     while (i < n && s[i] == ' ') i++;
 
-    // 2. Check for optional '+' or '-' sign
     if (i < n && (s[i] == '+' || s[i] == '-')) {
         sign = (s[i] == '-') ? -1 : 1;
         i++;
     }
 
-    // 3. Process digits and build the result
     while (i < n && isdigit(s[i])) {
         result = result * 10 + (s[i] - '0');
 
-        // 4. Clamp to int range if overflow occurs
         if (sign == 1 && result > INT_MAX) return INT_MAX;
         if (sign == -1 && -result < INT_MIN) return INT_MIN;
 
